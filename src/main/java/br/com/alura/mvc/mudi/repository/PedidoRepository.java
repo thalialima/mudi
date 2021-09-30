629a3a14adee7ec403f4e989433bd394f760a59e
thalialima <thaliairene20@gmail.com>
2021-09-30 14:32:56 -0300

    Integração com Spring Data

---
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -0,0 +1,25 @@
+package br.com.alura.mvc.mudi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+
+//Repository indica que a classe é um repositorio
+@Repository
+//O Spring Data JPA utiliza interfaces para fazer comunicação
+//Para que a interface funcione integrado com Spring Data
+//é necessário extender a interface JPARepository
+public interface PedidoRepository extends JpaRepository<Pedido, Long>{
+
+	// atributo necessário para a classe, gerenciado pelo Spring e injetado na classe
+	//como é um subprojeto JPA ele sabe utilizar o EntityManager
+	//@PersistenceContext
+	//private EntityManager em;
+
+	//Quando o Spring Data vê um método chamado findAll que recupera uma lista de pedidos
+	//ele sabe qual tipo de query deve fazer
+	//Implementase o método de acrordo com seu nome
+	//Esse método não é necessário pois ele já é herdado da interface JPARepository
+	//public List<Pedido> findAll();
+}
