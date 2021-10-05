9fbe743348a5f30cb7fdb99753e80d2e84e59d62
thalialima <thaliairene20@gmail.com>
2021-10-05 08:45:57 -0300

    Autenticação com Spring Security

--- src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
+++ src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
@@ -8,11 +8,7 @@ public class MudiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MudiApplication.class, args);
-		
-		
-		
-		
-		
+
 	}
 
 }
---
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package br.com.alura.mvc.mudi;
+
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+			.anyRequest().authenticated()
+		.and()
+		.formLogin(form -> form
+			//pede a url da página
+			.loginPage("/login")
+			.permitAll()
+		)
+		.logout(logout -> logout.logoutUrl("/logout"));
+	}
+	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+				//métod depreciado pois não é seguro usá-lo em produção
+			 User.withDefaultPasswordEncoder()
+				.username("Maria")
+				.password("123")
+				.roles("ADM")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+}
